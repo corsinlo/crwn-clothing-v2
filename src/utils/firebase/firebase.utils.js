e77d5d40363dbd8dff75d794ce63abf1a283d339
corsinlo <corsinlo@uc.edu>
2022-08-18 20:16:36 -0500

    Section 6 in progress.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,21 +6,22 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBSdjFG21x6Wcx7SF_6tELTDjOPXIV5WIM",
+  authDomain: "crwn-clothing-db-a51a5.firebaseapp.com",
+  projectId: "crwn-clothing-db-a51a5",
+  storageBucket: "crwn-clothing-db-a51a5.appspot.com",
+  messagingSenderId: "74291126244",
+  appId: "1:74291126244:web:a0d464d2c4d3141e79f1f1"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-  prompt: 'select_account',
+provider.setCustomParameters({ //describe how provider will behave
+  prompt: 'select_account', // forcing account selection
 });
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
